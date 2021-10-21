6bfbafd86e73144811f814387b881821076b0d5a
VincentCMLejeune <vincentcmlejeune@gmail.com>
2021-10-21 16:07:55 +0200

    Renamed attack functions

--- attackCheck.js
+++ attackCheck.js
@@ -1,6 +1,6 @@
 const dice = (faces = 20) => {return Math.floor(Math.random()*faces)+1}
 
-export default function attack(attacker, defender) {
+export default function attackCheck(attacker, defender) {
     console.log(`${attacker.name} tries to attack ${defender.name} !`)
     let attackRoll = (dice() - 10) * 10
     console.log(`Dice result : ${attackRoll}. ${attacker.name}'s combat : ${attacker.combat}. ${defender.name}'s combat : ${defender.combat}`)
