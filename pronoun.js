6ba236b49c2ded20389d299a35d48ddc280e8543
VincentCMLejeune <vincentcmlejeune@gmail.com>
2021-10-25 09:28:10 +0200

    Exported function for adding pronouns to the messages

---
+++ pronoun.js
@@ -0,0 +1,11 @@
+export default function pronoun(character, upperCase = true) {
+    if (attacker.gender == 'Female') {
+        pronoun = 'She'
+    }
+    else if (attacker.gender == 'Male') {
+        pronoun = 'He'
+    }
+    else {
+        pronoun = 'It'
+    }
+}
